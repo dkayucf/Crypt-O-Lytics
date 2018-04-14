d69475726d7518f0f95b1dd7731559c2d7a400e6
dkayucf <dkayucf@gmail.com>
2018-04-13 23:25:48 -0400

    finished getting crypotcurrency data to display in graph

--- assets/js/modules/ItemCtrl.js
+++ assets/js/modules/ItemCtrl.js
@@ -4,55 +4,82 @@ const ItemCtrl = (function(){
     //Public Methods
     return {
         mapCurrData: function(currData, chartStyle, getCurrSelects){
-            console.log(currData);
+
             Plotly.purge('currChartPlot');
 
             if(getCurrSelects.cryptoCurrency == 'currWeightAvg'){
                 console.log("HI KEVIN!!!!!");
                 ///////////// HERE KEVIN! ////////////////////
             } else {
-                trace1 = {
-                    x: stockData.chart.map(datum => datum.date),
-                    close: stockData.chart.map(datum => datum.close), 
-                    decreasing: {line: {color: '#7F7F7F'}}, 
-                    high: stockData.chart.map(datum => datum.high), 
-                    increasing: {line: {color: '#17BECF'}}, 
-                    line: {color: 'rgba(31,119,180,1)'}, 
-                    low: stockData.chart.map(datum => datum.low), 
-                    open: stockData.chart.map(datum => datum.open), 
-                    type: chartStyle, 
-                    xaxis: 'x', 
-                    yaxis: 'y'
-                };
+                let trace1 = {},
+                    data = [];
+                
+                
+                if(chartStyle == 'scatter'){
+                   
+                    trace1 = {
+                        type: chartStyle,
+                        mode: "lines",
+                        x: currData.map(datum => datum.time_period_start.split('T')[0]),
+                        y: currData.map(datum => datum.price_close),
+                        line: {color: '#17BECF'}
+                    }
+
+                    data = [trace1];
+
+                    let layout = {
+                        title: `${getCurrSelects.cryptoName} Currency Chart`
+                    };
+
+                    Plotly.plot('currChartPlot', data, layout); 
+                    
+                }else {
+                    trace1 = {
+                        x: currData.map(datum => datum.time_period_start.split('T')[0]),
+                        close: currData.map(datum => datum.price_close), 
+                        decreasing: {line: {color: '#7F7F7F'}}, 
+                        high: currData.map(datum => datum.price_high), 
+                        increasing: {line: {color: '#17BECF'}}, 
+                        line: {color: 'rgba(31,119,180,1)'}, 
+                        low: currData.map(datum => datum.price_low), 
+                        open: currData.map(datum => datum.price_open), 
+                        type: chartStyle, 
+                        xaxis: 'x', 
+                        yaxis: 'y'
+                    };
 
 
-                // data = [trace1];
+                     data = [trace1];
 
-                // let layout = {
-                //       dragmode: 'zoom', 
-                //       autosize: true,
-                //       margin: {
-                //         r: 10, 
-                //         t: 25, 
-                //         b: 40, 
-                //         l: 60
-                //       }, 
-                //       showlegend: false, 
-                //       xaxis: {
-                //         autorange: true, 
-                //         domain: [0, 1], 
-                //         range: [trace1.x[0], trace1.x[trace1.x.length-1]], 
-                //         rangeslider: {range: [trace1.x[0], trace1.x[trace1.x.length-1]]}, 
-                //         title: `${getStockSelects.stockName} Stock Chart`, 
-                //         type: 'date'
-                //       }, 
-                //       yaxis: {
-                //         autorange: true, 
-                //         domain: [0, 1], 
-                //         range: [Math.min(...trace1.low), Math.max(...trace1.high)],  
-                //         type: 'linear'
-                //       }
-                // };
+                     let layout = {
+                           dragmode: 'zoom', 
+                           autosize: true,
+                           margin: {
+                             r: 10, 
+                             t: 25, 
+                             b: 40, 
+                             l: 60
+                           }, 
+                           showlegend: false, 
+                           xaxis: {
+                             autorange: true, 
+                             domain: [0, 1], 
+                             range: [trace1.x[0], trace1.x[trace1.x.length-1]], 
+                             rangeslider: {range: [trace1.x[0], trace1.x[trace1.x.length-1]]}, 
+                             title: `${getCurrSelects.cryptoName} Currency Chart`, 
+                             type: 'date'
+                           }, 
+                           yaxis: {
+                             autorange: true, 
+                             domain: [0, 1], 
+                             range: [Math.min(...trace1.low), Math.max(...trace1.high)],  
+                             type: 'linear'
+                           }
+                     };
+
+                    Plotly.plot('currChartPlot', data, layout);   
+                }
+
             }
         },
 
