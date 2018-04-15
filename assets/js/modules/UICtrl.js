ef92a6bc6c30360a177c0e271307384f1614dac7
dkayucf <dkayucf@gmail.com>
2018-04-15 19:49:28 -0400

    reworked UI to adjust ticker tape and normalized data as well as displayed normalized data

--- assets/js/modules/UICtrl.js
+++ assets/js/modules/UICtrl.js
@@ -10,6 +10,9 @@ const UICtrl = (function(){
         //Inputs
         stockWeightAvg: '#stockWeightAvg',
         quickQuoteInput: '#quickQuoteInput',
+        hiddenCurrencyInput: '#hiddenCurrencyInput',
+        hiddenStockInput: '#hiddenStockInput',
+        
         
         //Selects
         techCompSelect: '.techCompSelect',
@@ -18,6 +21,8 @@ const UICtrl = (function(){
         cryptoCurrSelect: '.cryptoCurrSelect',
         cryptoTimeFrame: '.cryptoTimeFrame',
         cryptoChartStyle: '.cryptoChartStyle',
+        movingAverage: '.movingAverage',
+        upperIndicators: '.upperIndicators',
         
         //Tables
         quoteTbody1: '#quoteTbody1',
@@ -26,30 +31,52 @@ const UICtrl = (function(){
         //Divs
         stockChartCard: '#stockChartCard',
         currChartCard: '#currChartCard',
-        cardHeader: '.card-header'
+        cardHeader: '.card-header',
+        BTCQuote: '#BTCQuote',
+        ETHQuote: '#ETHQuote',
+        XRPQuote: '#XRPQuote',
+        BCHQuote: '#BCHQuote',
+        XLMQuote: '#XLMQuote',
+        LTCQuote: '#LTCQuote'
+        
         
     }
     
     //Public Methods
     return {
+        convertDate: function(date) {
+          var yyyy = date.getFullYear().toString();
+          var mm = (date.getMonth()+1).toString();
+          var dd  = date.getDate().toString();
+
+          var mmChars = mm.split('');
+          var ddChars = dd.split('');
+
+          return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
+        },
+        formatNumber: function(number){
+            return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        },
         displayStockQuote: function(quote){
             
             document.querySelector('#quoteModalTitle').innerHTML = `${quote.companyName} (${quote.symbol})`;
             
+            let marketCap = UICtrl.formatNumber(quote.marketCap);
+            console.log(marketCap);
             let html1 = `<tr>
-                <td>${quote.open}</td>
-                <td>${quote.latestPrice}</td>
-                <td>${quote.change}</td>
-                <td>${quote.changePercent}</td>
-                <td>${quote.high}</td>
-                <td>${quote.low}</td>
+                <td>$${quote.open}</td>
+                <td>$${quote.latestPrice}</td>
+                <td>$${quote.change}</td>
+                <td>${(quote.changePercent*100).toFixed(2)}%</td>
+                <td>$${quote.high}</td>
+                <td>$${quote.low}</td>
             </tr>`;
             
             let html2 = `<tr>
-                <td>${quote.marketCap}</td>
+                <td>$${marketCap}</td>
                 <td>${quote.peRatio}</td>
-                <td>${quote.week52High}</td>
-                <td>${quote.week52Low}</td>
+                <td>$${quote.week52High}</td>
+                <td>$${quote.week52Low}</td>
                 <td>${(quote.ytdChange * 100).toFixed(2)}%</td>
             </tr>`;
             
@@ -58,6 +85,7 @@ const UICtrl = (function(){
             
             $('#quickQuoteModal').modal('show');
         },
+        
         getStockSelects: function(){
             const stockSymbolSelect = document.querySelector(UISelectors.techCompSelect);
             const stockSymbolSelectValue = stockSymbolSelect.options[stockSymbolSelect.selectedIndex].value;
@@ -65,6 +93,10 @@ const UICtrl = (function(){
             
             const stockTimeFrame = document.querySelector(UISelectors.techTimeFrame);
             const stockTimeFrameValue = stockTimeFrame.options[stockTimeFrame.selectedIndex].value;
+            
+            const hiddenStockInput = document.querySelector(UISelectors.hiddenStockInput);
+            hiddenStockInput.value = stockSymbolSelectValue;
+            hiddenStockInput.setAttribute('timeFrame', `${stockTimeFrameValue}`);
             
             return {
                 stockSymbol: stockSymbolSelectValue,
@@ -80,6 +112,10 @@ const UICtrl = (function(){
             const cryptoTimeFrame = document.querySelector(UISelectors.cryptoTimeFrame);
             const cryptoTimeFrameValue = cryptoTimeFrame.options[cryptoTimeFrame.selectedIndex].value;
 
+            const hiddenCurrInput = document.querySelector(UISelectors.hiddenCurrencyInput);
+            hiddenCurrInput.value = cryptoSelectValue;
+            hiddenCurrInput.setAttribute('timeFrame', `${cryptoTimeFrameValue}`);
+            
             return {
                 cryptoCurrency: cryptoSelectValue,
                 cryptoCurrTimeFrame: cryptoTimeFrameValue,
@@ -97,14 +133,12 @@ const UICtrl = (function(){
         },
         toggleMenuState: (menuToggle) =>{
             if(menuToggle.classList.contains('addChevron')){
-                console.log(!menuToggle.parentElement.parentElement.classList.contains('active'));
                 if(!menuToggle.parentElement.parentElement.classList.contains('active')){
                     menuToggle.parentElement.parentElement.classList = 'card-header panel-heading bg-secondary active';
                 }else if (menuToggle.parentElement.parentElement.classList.contains('active')) {
                     menuToggle.parentElement.parentElement.classList = 'card-header panel-heading bg-dark';
                 }
             }
-            
             
         },
         resetSelects: () => {
